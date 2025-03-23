b8241e89a659bcf9790b1990c3bacbb70b080a70
Sageb25 <mtwaley25@gmail.com>
2025-03-23 18:52:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Mtwaley bot', // Your name
+  ownerNumber: '14703105589', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
@@ -11,3 +11,7 @@ const settings = {
 };
 
 module.exports = settings;
+
+
+
+
